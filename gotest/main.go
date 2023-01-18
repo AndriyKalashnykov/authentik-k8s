c81c77a93a4883cc2af3e8eba610644da5493a7a
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2023-01-17 22:28:53 -0500

    fix: updates
    Signed-off-by: Andriy Kalashnykov <AndriyKalashnykov@gmail.com>

--- gotest/main.go
+++ gotest/main.go
@@ -10,15 +10,6 @@ import (
 	api "goauthentik.io/api/v3"
 )
 
-// APIClient Hold the API Client and any relevant configuration
-type APIClient struct {
-	client *api.APIClient
-}
-type tracingTransport struct {
-	inner http.RoundTripper
-	ctx   context.Context
-}
-
 // GetTLSTransport Get a TLS transport instance, that skips verification if configured via environment variables.
 func GetTLSTransport(insecure bool) http.RoundTripper {
 	tlsTransport, err := httptransport.TLSTransport(httptransport.TLSClientOptions{
@@ -49,23 +40,36 @@ func main() {
 		Transport: GetTLSTransport(true),
 	}
 
-	identifier := "id"
-	user := 1
-	expiring := false
+	//identifier := "akadmin"
+	//user := 1
+	//expiring := false
 
-	tokenRequest := api.TokenRequest{
-		Identifier: identifier,
-		User:       IntToPointer(user),
-		Expiring:   BoolToPointer(expiring),
-	}
+	//tokenRequest := api.TokenRequest{
+	//	Identifier: identifier,
+	//	User:       IntToPointer(user),
+	//	Expiring:   BoolToPointer(expiring),
+	//}
+	//
+	//intent := api.IntentEnum(api.INTENTENUM_API)
+	//tokenRequest.Intent = &intent
+	//
+	//fmt.Println(tokenRequest)
 
-	fmt.Println(tokenRequest)
-
-	//config.AddDefaultHeader("Authorization", fmt.Sprintf("Bearer %s", token))
+	token := "ak-outpost-b253971d-d4f5-4ae0-bf5a-cfe854c02462-api"
+	config.AddDefaultHeader("Authorization", fmt.Sprintf("Bearer %s", token))
 
 	apiClient := api.NewAPIClient(config)
 
 	//rootConfig, _, err := apiClient.RootApi.RootConfigRetrieve(context.Background()).Execute()
+	//if err != nil {
+	//	fmt.Fprintf(os.Stderr, "Error when calling `AdminApi.AdminAppsList``: %v\n", err)
+	//	fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", rootConfig)
+	//}
+	//fmt.Fprintf(os.Stdout, "Response from `AdminApi.AdminAppsList`: %v\n", rootConfig)
+
+	list, rsp, err := apiClient.Oauth2Api.Oauth2AuthorizationCodesList(context.Background()).Execute()
+	fmt.Fprintf(os.Stdout, "list`: %v\n", list)
+	fmt.Fprintf(os.Stdout, "rsp`: %v\n", rsp)
 
 	resp, r, err := apiClient.AdminApi.AdminAppsList(context.Background()).Execute()
 	if err != nil {
