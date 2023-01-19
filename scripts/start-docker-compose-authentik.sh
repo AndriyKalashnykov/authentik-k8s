f3933c271c840ff25e37b6c586f14185b4e01bc5
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2023-01-19 12:39:44 -0500

    fix: updates
    Signed-off-by: Andriy Kalashnykov <AndriyKalashnykov@gmail.com>

---
+++ scripts/start-docker-compose-authentik.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+LAUNCH_DIR=$(pwd); SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"; cd $SCRIPT_DIR; cd ..; SCRIPT_PARENT_DIR=$(pwd);
+
+cd $SCRIPT_PARENT_DIR
+
+
+
+cd $LAUNCH_DIR
