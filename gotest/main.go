9f77386ed38db5ea54001cc567cd04dddd779037
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2023-01-20 10:32:09 -0500

    fix: updated comments
    Signed-off-by: Andriy Kalashnykov <AndriyKalashnykov@gmail.com>

--- gotest/main.go
+++ gotest/main.go
@@ -27,8 +27,8 @@ func main() {
 	akadminConfig := authentik.CreateConfiguration(AuthentikServerScheme, AuthentikServerHost, AuthentikBootstrapToken)
 	akadminApiClient := api.NewAPIClient(akadminConfig)
 
-	// create a group
-	// will create new QleetOS with different pk
+	// create QleetOS group
+	// will create new QleetOS group with different pk
 	grp, _, err := authentik.CreateGroup(ctx, akadminApiClient, QleetOSGroupName, QleetOSGroupIsSuperUser)
 	if err != nil {
 		log.Panicf("error: %v", err)
@@ -36,7 +36,7 @@ func main() {
 	groupUID := grp.Pk
 	log.Printf("groupUID``: %v\n", groupUID)
 
-	// create a user and include it to previously created group
+	// create qleetctl user and assign it to previously created group
 	usr, _, err := authentik.CreateUser(ctx, akadminApiClient, groupUID, QleetctlUser, UsersPath)
 	if err != nil {
 		log.Panicf("error: %v", err)
@@ -44,7 +44,7 @@ func main() {
 	userUID := usr.Pk
 	log.Printf("userUID``: %v\n", userUID)
 
-	// create user's password
+	// create qleetctl user's password
 	resp, err := authentik.UpdateUserPassword(ctx, akadminApiClient, userUID, QleetctlUserPwd)
 	if err != nil {
 		log.Panicf("error: %v", err)
@@ -53,7 +53,7 @@ func main() {
 
 	}
 
-	// create Oauth token
+	// create qleetctl user's OAuth token
 	token, resp, err := authentik.CreateUserToken(ctx, akadminApiClient, userUID, QleetctlTokenIdentifier, QleetctlTokenIdentifierDescription)
 	if err != nil {
 		log.Panicf("error: %v", err)
@@ -62,7 +62,7 @@ func main() {
 		log.Printf("Token: %v", token.Pk)
 	}
 
-	// retrieve Oauth token
+	// retrieve qleetctl user's OQuth token
 	tv, _, err := authentik.RetrieveUserToken(ctx, akadminApiClient, QleetctlTokenIdentifier)
 	if err != nil {
 		log.Panicf("error: %v", err)
